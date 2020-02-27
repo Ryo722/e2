932836de4462df35738a1b18a87c29ec4e0cc7e8
hana <hana05ry@icloud.com>
2020-02-27 10:14:33 +0000

    p163 メール機能は実装

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: 'smtp.gmail.com',
+    port: '587',
+    domain: 'smtp.gmail.com',
+    authentication: 'plain',
+    user_name: ENV['MAIL_USER_NAME'],
+    password: ENV['MAIL_PASSWORD'],
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
